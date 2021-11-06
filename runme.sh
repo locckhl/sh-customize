58a3c9b69e9d14c6e9b95619c9596b0af9861a6a
root <root@LAPTOP-VMCCV01G.localdomain>
2021-11-06 21:40:29 +0700

    fix path

--- runme.sh
+++ runme.sh
@@ -6,11 +6,13 @@ chmod +x /usr/local/bin/oh-my-posh
 # Download the themes
 mkdir ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+
+# 
 unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.json
 rm ~/.poshthemes/themes.zip
 
 # Change theme
-echo 'eval "$(oh-my-posh --init --shell bash --config ~/jandedobbeleer.omp.json)"' >> ~/.bashrc
+echo 'eval "$(oh-my-posh --init --shell bash --config ~/.poshthemes/jandedobbeleer.omp.json)"' >> ~/.bashrc
 
 . ~/.bashrc
