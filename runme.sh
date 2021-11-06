03f0c2b61349858f7f1d07b11d4eaedf451b1782
root <root@LAPTOP-VMCCV01G.localdomain>
2021-11-06 22:08:48 +0700

    add install unzip

--- runme.sh
+++ runme.sh
@@ -7,7 +7,17 @@ chmod +x /usr/local/bin/oh-my-posh
 mkdir ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
 
-# 
+# Install unzip if not found
+
+REQUIRED_PKG="unzip"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  sudo apt-get --yes install $REQUIRED_PKG
+fi
+
+ 
 unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.json
 rm ~/.poshthemes/themes.zip
